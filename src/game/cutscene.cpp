f3c8be0a6624b60ff420d284640635ecc1b2f652
Ian <ianw3214@gmail.com>
2018-10-04 14:06:46 -0400

    Add cutscene class interface to be implemented

---
+++ src/game/cutscene.cpp
@@ -0,0 +1,21 @@
+#include "cutscene.hpp"
+
+Cutscene::Cutscene(State * state) {
+
+}
+
+Cutscene::~Cutscene() {
+
+}
+
+void Cutscene::handleEvent(SDL_Event e) {
+
+}
+
+void Cutscene::update(int delta) {
+
+}
+
+void Cutscene::render() {
+
+}
---
+++ src/game/cutscene.hpp
@@ -0,0 +1,31 @@
+/**
+  *		Cutscene game state
+  *			- Set the next scene to transition to
+  *			- Store a list of images(sprites) to show in the cutscene
+  *			- Handle user input (mouse click or button press) to move between images
+  *		
+  *		Future goals
+  *			- Load cutscene data from file
+  *			- Animated sprites in cutscenes
+  *			- Text rendering in cutscenes
+  *			- Sprite movement in cutscenes
+  */
+#pragma once
+
+#include "../engine/core.hpp"
+
+class Cutscene : public State {
+
+public:
+
+	// The constructor takes the next State to transition to as input
+	Cutscene(State * state);
+	~Cutscene();
+
+	void handleEvent(SDL_Event e);
+	void update(int delta);
+	void render();
+
+private:
+
+};
