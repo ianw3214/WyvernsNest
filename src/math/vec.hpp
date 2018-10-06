16b64b8f9f7e42084cfa5180c8dc7ab3e478b264
Darren Moore <moodarren@gmail.com>
2018-10-06 17:06:06 -0400

    const dereference operator correctly returns const pointer to data

--- src/math/vec.hpp
+++ src/math/vec.hpp
@@ -10,16 +10,17 @@ struct Vec2 {
 	T& u() { return m_values[0]; }
 	T& v() { return m_values[1]; }
 
-	T operator [] (unsigned i) const { return this->m_values[i]; }
-	T & operator [] (unsigned i) { return this->m_values[i]; }
-	operator T*() const { return this->m_values; }
-
 	// Constructors
 	Vec2() {}
 	Vec2(T v1, T v2) {
 		m_values[0] = v1;
 		m_values[1] = v2;
 	}
+
+	// Operators
+	T operator [] (unsigned i) const { return this->m_values[i]; }
+	T& operator [] (unsigned i) { return this->m_values[i]; }
+	T const * operator *() const { return this->m_values; } // Used to pass raw data
 
 private:
 	T m_values[2];
@@ -41,10 +42,6 @@ struct Vec3 {
 	T& g() { return m_values[1]; }
 	T& b() { return m_values[2]; }
 
-	T operator [] (unsigned i) const { return this->m_values[i]; }
-	T & operator [] (unsigned i) { return this->m_values[i]; }
-	operator T*() const { return this->m_values; }
-
 	// Constructors
 	Vec3() {}
 	Vec3(T v1, T v2, T v3) {
@@ -52,6 +49,11 @@ struct Vec3 {
 		m_values[1] = v2;
 		m_values[2] = v3;
 	}
+
+	// Operators
+	T operator [] (unsigned i) const { return this->m_values[i]; }
+	T& operator [] (unsigned i) { return this->m_values[i]; }
+	T const * operator *() const { return this->m_values; }	// Used to pass raw data
 
 private:
 	T m_values[3];
