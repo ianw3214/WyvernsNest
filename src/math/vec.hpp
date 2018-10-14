333f47ba71b310b780f38f28f366cf044a9b6144
SeanWiz <triclopszx@gmail.com>
2018-10-14 18:59:26 -0400

    Added methods
    hopefully I have this down now

--- src/math/vec.hpp
+++ src/math/vec.hpp
@@ -56,6 +56,26 @@ struct Vec2 {
 		return *this;
 	}
 
+    T dot(Vec2 v) {
+        T x = this->m_values[0] * v->m_values[0];
+        T y = this->m_values[1] * v->m_values[1];
+
+        return x + y;
+    }
+    T norm() {
+        T x = pow(this->m_values[0], 2);
+        T y = pow(this->m_values[1], 2);
+
+        return sqrt(x + y);
+    }
+    Vec2 normalized() {
+        T x = this->m_values[0] * abs(this->m_values[0]);
+        T y = this->m_values[1] * abs(this->m_values[1]);
+        Vec2 n(x, y);
+
+        return n;
+    }
+
 
 private:
 	T m_values[2];
@@ -146,6 +166,31 @@ struct Vec3 {
 		(*this)[2] -= rhs[2];
 		return *this;
 	}
+
+    T dot(Vec3 v) {
+        T x = this->m_values[0] * v->m_values[0];
+        T y = this->m_values[1] * v->m_values[1];
+        T z = this->m_values[2] * v->m_values[2];
+
+        return x + y + z;
+    }
+
+    T norm() {
+        T x = pow(this->m_values[0], 2);
+        T y = pow(this->m_values[1], 2);
+        T z = pow(this->m_values[2], 2);
+
+        return sqrt(x + y + z);
+    }
+
+    Vec3 normalized() {
+        T x = this->m_values[0] * abs(this->m_values[0]);
+        T y = this->m_values[1] * abs(this->m_values[1]);
+        T z = this->m_values[2] * abs(this->m_values[2]);
+        Vec3 n(x, y, z);
+
+        return n;
+    }
 
 
 private:
