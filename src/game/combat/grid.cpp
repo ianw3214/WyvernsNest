598df78bcd1d19bbaea9c3f34ba4c4ad985e8e8a
Ian <ianw3214@gmail.com>
2018-10-17 14:20:18 -0400

    Move grid sprites to class property to avoid repeated instantiations every frame
    Clean up code structure of grid class

--- src/game/combat/grid.cpp
+++ src/game/combat/grid.cpp
@@ -1,7 +1,17 @@
 #include "grid.hpp"
 
-Grid::Grid() {
-
+Grid::Grid() :
+	tilemap(DEFAULT_TILEMAP),
+	map_width(DEFAULT_MAP_WIDTH),
+	map_height(DEFAULT_MAP_HEIGHT),
+	tile1("res/test.png"), 
+	tile2("res/test2.png"), 
+	selected("res/test3.png") 
+{
+	// Initialize the tile sprites to the tile width/height
+	tile1.setSize(TILE_WIDTH, TILE_HEIGHT);
+	tile2.setSize(TILE_WIDTH, TILE_HEIGHT);
+	selected.setSize(TILE_WIDTH, TILE_HEIGHT);
 }
 
 Grid::~Grid() {
@@ -12,51 +22,42 @@ void Grid::update()
 {
 	//mouseOn = GetCursorPos(&mouse);
 	SDL_GetMouseState(&mouseX, &mouseY);
+	mousePos = getMouseToGrid();
 }
 
 
 ScreenCoord Grid::getMouseToGrid()
 {
-	int x = floor(mouseX / 110);
-	int y = floor(mouseY / 110);
+	int x = static_cast<int>(floor(mouseX / 110));
+	int y = static_cast<int>(floor(mouseY / 110));
 
 	return ScreenCoord(x,y);
 }
 
 bool Grid::isMousePosValid()
 {
-	return mousePos.x() < x_ && mousePos.y() < y_;
+	return mousePos.x() < map_width && mousePos.y() < map_height;
 }
-
-
 
 
 void Grid::render()
 {
-	mousePos = getMouseToGrid();
+	for (int y = 0; y < map_height; y++) {
+		for (int x = 0; x < map_width; x++) {
+			if (tilemap[TILE_INDEX(x, y)] == 1) {
 
-	for (int y = 0; y < y_; y++) {
-		for (int x = 0; x < x_; x++) {
-			if (array[y][x] == 1) {
-
-				Sprite sprite("res/test.png");
-				sprite.setPos(100 * x + (x+1) * spacing, 620 - 100 * y - (y + 1) * spacing);
-				sprite.setSize(width, height);
-				sprite.render();
+				tile1.setPos(100 * x + (x+1) * spacing, 620 - 100 * y - (y + 1) * spacing);
+				tile1.render();
 			}
 			else {
-				Sprite sprite("res/test2.png");
-				sprite.setPos(100 * x + (x + 1) * spacing, 620 - 100 * y - (y + 1) * spacing);
-				sprite.setSize(width, height);
-				sprite.render();
+				tile2.setPos(100 * x + (x + 1) * spacing, 620 - 100 * y - (y + 1) * spacing);
+				tile2.render();
 			}
 
 			//selected square
 			if (y == mousePos.y() && x == mousePos.x()) {
-				Sprite sprite("res/test3.png");
-				sprite.setPos(100 * x + (x + 1) * spacing, 620 - 100 * y - (y + 1) * spacing);
-				sprite.setSize(width, height);
-				sprite.render();
+				selected.setPos(100 * x + (x + 1) * spacing, 620 - 100 * y - (y + 1) * spacing);
+				selected.render();
 			}
 		}
 	}
--- src/game/combat/grid.hpp
+++ src/game/combat/grid.hpp
@@ -3,8 +3,16 @@
 #include "../../engine/core.hpp"
 #include "Windows.h"
 #include "../../math/vec.hpp"
-#include <math.h> 
+#include <cmath> 
 
+#define TILE_WIDTH 100
+#define TILE_HEIGHT 100
+
+#define DEFAULT_MAP_WIDTH 4
+#define DEFAULT_MAP_HEIGHT 3
+
+#define DEFAULT_TILEMAP { 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0 }
+#define TILE_INDEX(x, y) (y * map_width + x)
 
 class Grid {	
 
@@ -19,17 +27,10 @@ public:
 
 	bool isMousePosValid();
 
-	int array[3][4] = {
-		{1, 1, 0, 0} ,   
-		{1, 1, 1, 1} ,   
-		{0, 1, 0, 0}   
-	};
+	std::vector<int> tilemap;
 
-	int y_ = 3;
-	int x_ = 4;
-
-	int width = 100;
-	int height = 100;
+	int map_width;
+	int map_height;
 
 	int spacing = 10;
 
@@ -40,5 +41,9 @@ public:
 	ScreenCoord mousePos;
 
 private:
+
+	Sprite tile1;
+	Sprite tile2;
+	Sprite selected;
 
 };
