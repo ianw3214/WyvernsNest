4584b4a180ec4822b64aa2067ee079178650f5ef
Darren Moore <moodarren@gmail.com>
2018-10-20 12:59:48 -0400

    Add text renderer.

---
+++ src/engine/text/textRenderer.cpp
@@ -0,0 +1,134 @@
+#include "textRenderer.hpp"
+
+const char* text_vertex =
+    "#version 330 core\n"
+	"layout(location = 0) in vec4 position; /* vec2 position, vec2 texCoord */"
+    "out vec2 texCoord;"
+    "mat4 ortho(float left, float right, float bottom, float top) {"
+    "return mat4(vec4(2.0 / (right - left), 0, 0, 0), vec4(0, 2.0 / top - bottom, 0, 0), vec4(0, 0, -1, 0), vec4(-(right + left) / (right - left), -(top + bottom) / (top - bottom), 0, 1));}"
+	"void main() {"
+		"gl_Position = ortho(0, 1280, 0, 720) * vec4(position.xy, 0, 1);"
+        "texCoord = position.zw;"
+	"}";
+
+const char* text_fragment =
+    "#version 330 core\n"
+    "in vec2 texCoord;"
+	"out vec4 colour;"
+    "uniform sampler2D textTexture;"
+    "uniform vec3 textColour;"
+	"void main() {"
+		"colour = vec4(textColour, 1.0) * vec4(1.0, 1.0, 1.0, texture(textTexture, texCoord).r);"
+	"}";
+
+TextRenderer::TextRenderer(const std::string& fontPath, int size, Vec2<int> windowSize) :
+    m_shader(text_vertex, text_fragment), m_fontSize(size), m_windowSize(windowSize){
+    Font font(fontPath, size);
+
+	m_text.resize(128);
+	for (char c = 0; c < 127; ++c) {
+		m_text[c] = font.generateCharacter(c);
+	}
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+void TextRenderer::render(std::string s, ScreenCoord pos, Vec3<float> colour, TextRenderer::hAlign ah, TextRenderer::vAlign av) {
+	renderLine(s, pos, colour, ah, av);	// Will add multiline support in future
+}
+
+void TextRenderer::renderLine(std::string s, ScreenCoord pos, Vec3<float> colour, TextRenderer::hAlign ah, TextRenderer::vAlign av) {
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    m_shader.bind();
+    m_shader.setUniform3f("textColour", colour.r(), colour.g(), colour.b());
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO);
+
+	Vec2<int> sSize = computeLineSize(s);
+	GLfloat x_begin;
+	switch (ah) {
+	case TextRenderer::hAlign::left:
+		x_begin = pos.x();
+		break;
+	case TextRenderer::hAlign::centre:
+		x_begin = pos.x() - sSize.x() / 2;
+		break;
+	case TextRenderer::hAlign::right:
+		x_begin = pos.x() - sSize.x();
+		break;
+	}
+	
+	GLfloat y_begin;
+	switch (av) {
+	case TextRenderer::vAlign::top:
+		y_begin = pos.y();
+		break;
+	case TextRenderer::vAlign::middle:
+		y_begin = pos.y() - sSize.y() / 2;
+		break;
+	case TextRenderer::vAlign::bottom:
+		y_begin = pos.y() - sSize.y();
+		break;
+	}
+
+	GLfloat x = static_cast<GLfloat>(x_begin);
+	GLfloat y = -static_cast<GLfloat>(y_begin);	// Sign error somewhere below...
+	
+
+    for(char c : s) {
+		Character ch = m_text[c];
+        GLfloat xpos = static_cast<GLfloat>(x + ch.bearing.x());
+        GLfloat ypos = static_cast<GLfloat>(y + ch.bearing.y() - ch.size.y());
+
+        GLfloat w = static_cast<GLfloat>(ch.size.x());
+        GLfloat h = static_cast<GLfloat>(ch.size.y());
+			
+		int winHeight = m_windowSize.y();
+        // Update VBO for each character
+        GLfloat vertices[] = {
+			// (winHeight - h) makes the text start from the top left corner 
+             xpos,     ypos + h + (winHeight - h),   0.0, 0.0 ,
+             xpos,     ypos + (winHeight - h),       0.0, 1.0 ,
+             xpos + w, ypos + (winHeight - h),       1.0, 1.0 ,
+
+             xpos,     ypos + h + (winHeight - h),   0.0, 0.0 ,
+             xpos + w, ypos + (winHeight - h),       1.0, 1.0 ,
+             xpos + w, ypos + h + (winHeight - h),   1.0, 0.0
+        };
+
+        // Render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.textureID);
+        // Update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); 
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // Render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.advance >> 6); // Bitshift by 6 to get value in pixels (2^6 = 64)
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    m_shader.unbind();
+}
+
+Vec2<int> TextRenderer::computeLineSize(std::string s) {
+	int x = 0;
+	for (char c : s) {
+		Character ch = m_text[c];
+		x += (ch.advance >> 6);	// Bitshift by 6 to get value in pixels (2^6 = 64)
+	}
+
+	int y = m_fontSize;
+	return Vec2<int>(x, y);
+}
---
+++ src/engine/text/textRenderer.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include <GL/glew.h>
+
+#include "font.hpp"
+#include "../opengl/glwrappers.hpp"
+
+/**
+ * Basic usage of Text
+ * 
+ * Create a Text object with the following parameters:
+ *  - text: The text that will be rendered
+ *  - fontPath: The path to the font used to render the text
+ *  - size: The desired text size
+ *  - containerSize: The size of the region in which text will be rendered
+ * 
+ * Displaying text (render method):
+ *  - colour: The desired colour of the text
+ *  - alignment: The desired alignment of the text (left, center, right)
+ *  - location: The location for the text to be rendered. (0,0) is the top left corner of the window
+ *  and (width, height) is the bottom right.
+ * 
+ * Example:
+ *  Text text("this is some text", "res/test_font.ttf", 48, Vec2<int>(Core::windowWidth(), Core::windowHeight()))
+ *  ...
+ *  text.render(Vec3<float>(1, 1, 1), TextAlignment::center, ScreenCoord(150, 0));
+ *
+ * This creates a text object with the "test_font" font", "this is some text" as the text to be rendered, a font size
+ * of 48 and a container that is the size of the window. The colour of the text is white, it is centered, and it begins
+ * 150 pixels from the left side of the window and at the top of the window.
+ **/
+
+class TextRenderer {
+public:
+
+	enum hAlign { left, centre, right };
+	enum vAlign { top, middle, bottom };
+
+    TextRenderer(const std::string& fontPath, int size, Vec2<int> windowSize);
+
+    void render(std::string s, ScreenCoord pos, Vec3<float> colour = Vec3<float>(1,0,1), hAlign ha = hAlign::left, vAlign va = vAlign::top);
+private:
+
+	void renderLine(std::string s, ScreenCoord pos, Vec3<float> colour = Vec3<float>(1, 0, 1), hAlign ha = hAlign::left, vAlign va = vAlign::top);
+    std::vector<Character> m_text;
+	Vec2<int> computeLineSize(std::string s);
+
+	int m_fontSize;
+	Vec2<int> m_windowSize;
+    Shader m_shader;
+    GLuint VAO, VBO;
+};
