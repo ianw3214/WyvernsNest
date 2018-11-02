af230e2a16db3ded850b80647e2f8a54e2b92c51
Loic Nassif <loic.nassif.lachapelle@mail.utoronto.ca>
2018-11-01 20:08:20 -0400

    tested and working basic implementation of cutscene

--- src/game/cutscene.cpp
+++ src/game/cutscene.cpp
@@ -1,18 +1,35 @@
 #include "cutscene.hpp"
 
+#include <iostream>
+
 Cutscene::Cutscene(State * state) {
+
+	// For testing purposes populate sprites with temp images
+	m_sprites.push_back(new Sprite("res/test.png"));
+	m_sprites.push_back(new Sprite("res/test2.png"));
+	m_sprites.push_back(new Sprite("res/test3.png"));
+	m_sprites.push_back(new Sprite("res/test4.png"));
+	m_sprites.push_back(new Sprite("res/test5.png"));
+
 	m_position = 0;
 	m_curr_img = m_sprites[m_position];
 	m_state = state;
+
 }
 
 Cutscene::~Cutscene() {
-	changeState(m_state);
+	
 }
 
 void Cutscene::handleEvent(const SDL_Event& e) {
-	if (e.type == SDL_SCANCODE_RIGHT && m_position < size(m_sprites)-1) m_curr_img = m_sprites[m_position++];
-	else if (e.type == SDL_SCANCODE_LEFT && m_position > 0) m_curr_img = m_sprites[m_position--];
+	if (e.type == SDL_KEYDOWN) {
+		if (e.key.keysym.sym == SDLK_a && m_position < size(m_sprites) - 1) {
+			m_curr_img = m_sprites[m_position++];
+		}
+		else if (e.key.keysym.sym == SDLK_a && m_position == size(m_sprites) - 1) {
+			changeState(m_state);
+		}
+	}
 }
 
 void Cutscene::update(int delta) {
