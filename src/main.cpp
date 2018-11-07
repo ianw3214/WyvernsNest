40995a92af68786341ef6e1aaaaec7ad9a60b603
Josh Martine <josh.martine@mail.utoronto.ca>
2018-11-06 19:01:38 -0500

    Updated mixer example

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,9 @@ int main(int argc, char* argv[]) {
 	}
 
 	// Example mixer usage
-	Mixer mixer;
-	mixer.loadAudio("res/music/track1.wav", AudioType::Music);
-	mixer.playAudio("res/music/track1.wav", 1, 0.8f);
+	Mixer *mixer = Engine::get_instance().getMixer();
+	mixer->loadAudio("res/music/track1.wav", AudioType::Music);
+	mixer->playAudio("res/music/track1.wav", 1, 0.8f);
 
 	// Core::setDebugMode(true);
 
