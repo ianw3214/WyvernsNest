1b153203a4fc5d14894c284cae26ceca7e7df6c8
Loic Nassif <loic.nassif.lachapelle@mail.utoronto.ca>
2018-11-10 01:16:07 -0500

    user can specify time, first image now takes the same amount of time

--- src/game/cutscene.hpp
+++ src/game/cutscene.hpp
@@ -20,7 +20,7 @@ class Cutscene : public State {
 public:
 
 	// The constructor takes the next State to transition to as input
-	Cutscene(State * state);
+	Cutscene(State * state, int scene_time=5);
 	~Cutscene();
 
 	void handleEvent(const SDL_Event& e) override;
@@ -30,6 +30,7 @@ public:
 private:
 	State *m_state;
 	std::clock_t m_start;
+	int m_scene_time;
 	Sprite *m_curr_img;
 	unsigned int m_position;
 	std::vector<Sprite *> m_sprites;
--- src/main.cpp
+++ src/main.cpp
@@ -4,34 +4,32 @@
 
 #include "engine/core.hpp"
 #include "game/menu.hpp"
-#include "game/cutscene.hpp"
 
 #include "engine/mixer.hpp"
 
 int main(int argc, char* argv[]) {
 
-    // Initialize the engine
-    if (!Core::init("Wyverns Nest", 1280, 720)) {
-        std::cerr << "Initialization failed\n";
-        return 1;
-    }
+	// Initialize the engine
+	if (!Core::init("Wyverns Nest", 1280, 720)) {
+		std::cerr << "Initialization failed\n";
+		return 1;
+	}
 
-    // Example mixer usage
-    Core::Mixer::loadAudio("res/music/track1.wav", AudioType::Music);
-    Core::Mixer::playAudio("res/music/track1.wav", 1, 0.8f);
+	// Example mixer usage
+	Core::Mixer::loadAudio("res/music/track1.wav", AudioType::Music);
+	Core::Mixer::playAudio("res/music/track1.wav", 1, 0.8f);
 
-    // Core::setDebugMode(true);
+	// Core::setDebugMode(true);
 
-    // Combat * state = new Combat();
-    Menu * state = new Menu();
-    // Cutscene *state = new Cutscene(state1); // Testing cutscene
-    Core::setState(state);
+	// Combat * state = new Combat();
+	Menu * state = new Menu();
+	Core::setState(state);
 
-    // Main engine loop
-    while (Core::running()) {
-        Core::update();
-    }
+	// Main engine loop
+	while (Core::running()) {
+		Core::update();
+	}
 
-    return 0;
+	return 0;
 
 }
